d17deb4e558b91b735b0101982ef2475645583e5
Meyers Arlean <marlean@at-j6.msk.21-school.ru>
2022-03-24 20:25:12 +0300

    day01finish

--- Day01/ex07/day01_ex07.sql
+++ Day01/ex07/day01_ex07.sql
@@ -1,4 +1,4 @@
-SELECT order_date, name || ' (age:'|| age || ')' AS personal_information
+SELECT order_date, name || ' (age:'|| age || ')' AS person_information
 FROM person_order
-JOIN person ON person_order.id = person.id
+JOIN person ON person_order.person_id = person.id
 ORDER BY 1, 2;
--- Day01/ex08/day01_ex08.sql
+++ Day01/ex08/day01_ex08.sql
@@ -1,4 +1,4 @@
-SELECT order_date, CONCAT(t.name, ' (age:', t.age, ')') AS personal_information
+SELECT order_date, CONCAT(t.name, ' (age:', t.age, ')') AS person_information
 FROM person_order
 NATURAL JOIN (SELECT id AS person_id, name, age FROM person) AS t
 ORDER BY 1, 2;
