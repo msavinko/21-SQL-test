ce6b1a090efb72dd8c119dd22e1be34e1781d33b
Meyers Arlean <marlean@ox-l3.msk.21-school.ru>
2022-04-02 14:29:29 +0300

    update

--- Day07/ex09/day07_ex09.sql
+++ Day07/ex09/day07_ex09.sql
@@ -1,7 +1,7 @@
 select person.address,
     round(max(age::numeric) - (min(age::numeric)/max(age::numeric)), 2) as formula,
     round(avg(age::numeric), 2) as average,
-    round(max(age::numeric) - min(age::numeric)/max(age::numeric), 2) > round(avg(age::numeric), 2)
+    round(max(age::numeric) - min(age::numeric)/max(age::numeric), 2) > round(avg(age::numeric), 2) as comparison
 from person
 group by person.address
 order by address;
